ef0d84207af5cf3ad966dc5b33f66ec8f40122ed
Pedro Wagner <pedrowagner0801@gmail.com>
2024-03-12 08:17:08 -0300

    Treino de modularização Java

---
+++ Pessoa/main.java
@@ -0,0 +1,7 @@
+class main{
+    public static void main(String[] args){
+        Pessoa p;
+        p = new Pessoa();
+        p.andar(2);
+    }
+}
---
+++ Pessoa/pessoa.java
@@ -0,0 +1,10 @@
+class Pessoa {
+    String nome;
+    String cpf;
+    int idade;
+
+    public static void andar (int i) {
+        String s = "andar " + String.valueOf(i) + " metros";  
+        System.out.println(s);
+    }
+}
